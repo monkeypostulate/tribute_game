a7ed5bcac6530bafb4544e02fcea031c444778d0
Abel Camacho <abel.camacho@uzh.ch>
2017-01-30 16:20:46 +0100

    tg

---
+++ RunWebShiny (Autosaved).R
@@ -0,0 +1,12 @@
+#runApp('/Users/cuatrooctavos/Dropbox/Web_agents')
+
+
+
+
+#runApp('/Users/cuatrooctavos/Desktop/world_map')
+
+library(shiny)
+folder<-getwd()
+runApp(folder)
+
+
---
+++ RunWebShiny.R
@@ -0,0 +1,11 @@
+#runApp('/Users/cuatrooctavos/Dropbox/Web_agents')
+
+
+
+
+#runApp('/Users/cuatrooctavos/Desktop/world_map')
+
+library(shiny)
+runApp('/Users/cuatrooctavos/Desktop/Introduction_1_2/Tribute_Model')
+
+
